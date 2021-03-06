e384c2b460f14b57f388537aa864de2a65f64ae6
Luis Pereira <luispereira.tkd@gmail.com>
2021-03-06 22:08:34 +0000

    Added system config to ackerman host

---
+++ home/.config/guix/config_ackerman.scm
@@ -0,0 +1,145 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+
+(use-modules (gnu)
+	     (srfi srfi-1)
+	     (gnu services desktop)
+	     (gnu services base)
+	     (gnu services cups)
+	     (gnu services pm)
+	     (gnu services docker)
+	     (gnu services virtualization)
+	     (gnu packages version-control)
+	     (gnu packages linux)
+	     (gnu packages mtools)
+	     (gnu packages audio)
+	     (gnu packages pulseaudio)
+	     (gnu packages bash)
+	     (gnu packages shells)
+	     (gnu packages xorg)
+	     (gnu packages gnome)
+	     (gnu packages cups)
+	     (gnu packages vim))
+(use-service-modules desktop networking ssh xorg)
+;; Import nonfree linux module.
+(use-modules (nongnu packages linux)
+	     (nongnu system linux-initrd))
+
+;; (define %my-desktop-services
+;;   (cons*
+;;    (service openssh-service-type)
+   ;; (set-xorg-configuration
+   ;;  (xorg-configuration
+   ;;   (keyboard-layout keyboard-layout)))
+   ;; (extra-special-file "/bin/bash" (file-append bash "/bin/bash"))
+   ;; ))
+(define %my-desktop-services
+  (modify-services %desktop-services
+		   (elogind-service-type config =>
+					 (elogind-configuration (inherit config)
+								(handle-lid-switch-external-power 'suspend)))
+		   ))
+
+(operating-system
+ (kernel linux)
+ (initrd microcode-initrd)
+ (firmware (list linux-firmware))
+ (locale "en_US.utf8")
+ (timezone "Europe/Lisbon")
+ (keyboard-layout (keyboard-layout "pt"))
+ (host-name "ackerman")
+ (groups
+  (append
+   (list
+    (user-group
+     (name "plugdev")))
+   %base-groups))
+ (users (cons* (user-account
+		(name "luis")
+		(comment "Luis Pereira")
+		(group "users")
+		(home-directory "/home/luis")
+		(supplementary-groups
+		 '("wheel" "netdev" "audio" "video" "plugdev" "input")))
+	       %base-user-accounts))
+ (packages
+  (append
+   (list (specification->package "openbox")
+	 (specification->package "nss-certs")
+	 zsh
+	 git
+	 ntfs-3g
+	 exfat-utils
+	 fuse-exfat
+	 vim
+	 bluez
+	 bluez-alsa
+	 pulseaudio
+	 tlp
+	 xf86-input-libinput
+	 (specification->package "gcc-toolchain")
+	 (specification->package "make")
+	 (specification->package "cmake")
+	 (specification->package "pkg-config")
+	 gvfs)
+   %base-packages))
+ ;; (service special-files-service-type 
+ ;; 	  `(("/bin/sh" ,(file-append bash "/bin/sh")) %base-services))
+    
+ ;; (services %my-desktop-services)
+ ;; (services
+ ;;  (append
+ ;;   (list (service openssh-service-type)
+ ;; 	 ;; (service slim-service-type)
+ ;; 	 ;; (remove (lambda (service)
+ ;; 	 ;; 	   (eq? (service-kind service) gdm-service-type))))
+ ;; 	 (set-xorg-configuration
+ ;; 	  (xorg-configuration
+ ;; 	   (keyboard-layout keyboard-layout))))
+ ;;   %desktop-services
+ ;;   (list (extra-special-file "/bin/bash" (file-append bash "/bin/bash")))
+ ;;   ))
+(services (cons* (service slim-service-type
+                              (slim-configuration
+                                (xorg-configuration
+                                  (xorg-configuration
+                                    (keyboard-layout keyboard-layout)
+                                    ))))
+                    (service tlp-service-type
+                             (tlp-configuration
+                                (cpu-boost-on-ac? #t)
+                                (wifi-pwr-on-bat? #t)))
+                    (service thermald-service-type)
+                    (service docker-service-type)
+                    (service libvirt-service-type
+                             (libvirt-configuration
+                              (unix-sock-group "libvirt")
+                              (tls-port "16555")))
+                    (service cups-service-type
+                             (cups-configuration
+                               (web-interface? #t)
+                               (extensions
+                                 (list cups-filters))))
+                    (bluetooth-service #:auto-enable? #t)
+		    (extra-special-file "/bin/bash" (file-append bash "/bin/bash"))
+		    (extra-special-file "/bin/zsh" (file-append zsh "/bin/zsh"))
+                    (remove (lambda (service)
+                                (eq? (service-kind service) gdm-service-type))
+                            %my-desktop-services)))
+ (bootloader
+    (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (target "/boot/efi")
+      (keyboard-layout keyboard-layout)))
+  (file-systems
+    (cons* (file-system
+             (mount-point "/")
+             (device
+               (uuid "66f32832-d2e1-4085-b95c-f7155e99f35b"
+                     'ext4))
+             (type "ext4"))
+           (file-system
+             (mount-point "/boot/efi")
+             (device (uuid "807C-CE39" 'fat32))
+             (type "vfat"))
+           %base-file-systems)))
