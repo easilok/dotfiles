6c667760a41781077d81929e36bb07b4dfc5c491
Luis Pereira <luispereira.tkd@gmail.com>
2024-12-29 19:41:42 +0000

    nvim: add description to lua source commands

--- nvim/.config/nvim/lua/easilok/init.lua
+++ nvim/.config/nvim/lua/easilok/init.lua
@@ -1,3 +1,4 @@
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+print("Loaded easilok")
+vim.keymap.set("n", "<space>sb", "<cmd>source %<CR>", { desc = "[S]ource [b]uffer" })
 -- vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>", { desc = "E[x]ecute selection" })
