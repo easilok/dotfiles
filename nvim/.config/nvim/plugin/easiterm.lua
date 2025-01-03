56fa180a0a7e5739cc1faf3ef3b37a69627e03d4
Luis Pereira <luispereira.tkd@gmail.com>
2025-01-03 23:36:36 +0000

    nvim: improved easiterm to force single instance

--- nvim/.config/nvim/plugin/easiterm.lua
+++ nvim/.config/nvim/plugin/easiterm.lua
@@ -1,32 +1,57 @@
-vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+local M = {}
+
+local state = {
+    job_id = 0,
+    term_buf = -1,
+    current_command = "",
+}
+
+M.open_term_split = function(detach)
+    if detach == nil then
+        detach = false
+    end
+
+    vim.cmd.vnew()
+    if not detach and vim.api.nvim_buf_is_valid(state.term_buf) then
+        vim.api.nvim_set_current_buf(state.term_buf)
+    else
+        vim.cmd.term()
+    end
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 20)
+
+    if not detach then
+        state.term_buf = vim.api.nvim_get_current_buf()
+        state.job_id = vim.bo.channel
+    end
+end
+
+vim.api.nvim_create_user_command("Easiterm", M.open_term_split, {})
+
+vim.api.nvim_create_user_command("EasitermDetach", function() M.open_term_split(true) end, {})
 
 vim.api.nvim_create_autocmd("TermOpen", {
-  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
+    group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
 })
 
-local job_id = 0
-vim.keymap.set("n", "<space>to", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 5)
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 
-  job_id = vim.bo.channel
-end, { desc = "[T]erminal [o]pen on split" })
+vim.keymap.set("n", "<space>to", M.open_term_split, { desc = "[T]erminal [o]pen on split" })
 
-local current_command = ""
 vim.keymap.set("n", "<space>ti", function()
-  current_command = vim.fn.input("Command: ")
+    state.current_command = vim.fn.input("Command: ")
 end, { desc = "[T]erminal [i]nput command" })
 
 vim.keymap.set("n", "<space>tx", function()
-  if current_command == "" then
-    current_command = vim.fn.input("Command: ")
-  end
+    if state.current_command == "" then
+        state.current_command = vim.fn.input("Command: ")
+    end
 
-  vim.fn.chansend(job_id, { current_command .. "\r\n" })
+    vim.fn.chansend(state.job_id, { state.current_command .. "\r\n" })
 end, { desc = "[T]erminal e[x]exute command" })
+
+return M
