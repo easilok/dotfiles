16a72d361764f03e29157de9079e576ed3b4909b
Luis Pereira <luispereira.tkd@gmail.com>
2025-02-24 09:44:43 +0000

    guix: add guix dev server config

---
+++ guix.dev-server/.config/guix/manifests/base.scm
@@ -0,0 +1,46 @@
+;; -*- mode: guix-scheme-*-
+
+(specifications->manifest
+ '(
+   "coreutils"
+   "gnupg"
+   "pinentry"
+
+   ;; These should be global?
+   "gcc-toolchain"
+   "make"
+   "libtool"
+   "binutils"
+   
+   ;; network tools
+   "curl"
+   "rsync"
+   "git"
+   "bind:utils" ;; nslookup and dig
+   "nss-certs"  ;; lets encrypt certs
+
+   ;; file mgmt
+   "unzip"
+   "zip"
+   "bash-completion"
+   "findutils"
+   "gzip"
+   "pbzip2" ;; multi core replacement for bzip2/bunzip2
+   "diffutils"
+
+   ;; system monitoring
+   "htop"
+   "strace"
+   "lsof"
+   "sysstat"
+
+   ;; console tools
+   "bc" ;; calculator
+   "password-store"
+   "jq"
+   "tmux"
+   "stow"
+   "eza"
+
+   ))
+
---
+++ guix.dev-server/.config/guix/manifests/development.scm
@@ -0,0 +1,20 @@
+;; -*- mode: guix-scheme-*-
+
+(specifications->manifest
+ '(
+     "neovim"
+     "tree-sitter-cli"
+     "python-pynvim"
+     "node"
+     "zsh"
+     "zsh-autosuggestions"
+     "zsh-syntax-highlighting"
+     "zsh-completions"
+     "starship"
+     "zoxide"
+     ;; "fzy"
+     "fzf"
+     "go-github-com-junegunn-fzf"
+     "ripgrep"
+     "git"
+  ))
