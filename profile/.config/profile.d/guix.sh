4c4a0b13b64181d8b54a74a4c2c0f8ac852dad3d
Luis Pereira <luispereira.tkd@gmail.com>
2025-02-26 16:50:49 +0000

    profile: add multifile loader

---
+++ profile/.config/profile.d/guix.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+	
+## Extra Guix profiles section
+# unset GUIX_PROFILE
+GUIX_EXTRA_PROFILES="$HOME/.guix-extra-profiles"
+if [ -d "$GUIX_EXTRA_PROFILES" ]; then
+    for i in $GUIX_EXTRA_PROFILES/*; do
+	profile=$i/$(basename "$i")
+	if [ -f "$profile"/etc/profile ]; then
+	    GUIX_PROFILE="$profile"
+	    . "$GUIX_PROFILE"/etc/profile
+	fi
+	if [ -d "$profile"/share/man ]; then
+        export MANPATH=$GUIX_PROFILE/share/man${MANPATH:+:}$MANPATH
+    fi
+	unset profile
+    done
+fi
+
+## Default Guix
+# export GUIX_PROFILE="$HOME/.guix-profile"
+# [ -d "$GUIX_PROFILE/etc/profile" ] && . "$GUIX_PROFILE/etc/profile"
+
+## SSL CA path set
+export SSL_CERT_DIR="$HOME/.guix-profile/etc/ssl/certs"
+export SSL_CERT_FILE="$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt"
+export GIT_SSL_CAINFO="$SSL_CERT_FILE"
