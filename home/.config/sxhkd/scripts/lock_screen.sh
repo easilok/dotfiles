bc348b855c9a049ed552a2005bc0e624ef1c8835
Luis Pereira <luispereira.tkd@gmail.com>
2025-06-17 09:09:59 +0100

    i3: add lock with image

--- home/.config/sxhkd/scripts/lock_screen.sh
+++ home/.config/sxhkd/scripts/lock_screen.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 if [ $(pgrep -f "xfce4-session") ]; then
   xflock4 
+elif [ -f "$(which i3lock-clock-image)" ]; then 
+    i3lock-clock-image
 elif [ -f "$(which i3lock-clock)" ]; then 
     i3lock-clock
 elif [ -f "$(which i3lock-fancy)" ]; then 
