71c2faff2f66d9062e42b158c641317ac6d612a2
Luis Pereira <luispereira.tkd@gmail.com>
2025-06-17 09:02:44 +0100

    dev: remove nvm and pyenv. using mise now

--- profile/.config/profile.d/98-dev.sh
+++ profile/.config/profile.d/98-dev.sh
@@ -8,11 +8,11 @@ if [ -d "$HOME/.pyenv" ]; then
     eval "$(pyenv init -)"
 fi
 
-export NVM_DIR="$HOME/.config/nvm"
-if [ -d $NVM_DIR ]; then
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-fi
+# export NVM_DIR="$HOME/.config/nvm"
+# if [ -d $NVM_DIR ]; then
+#     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# fi
 
 if [ -f "$HOME/.cargo/env" ]; then
     . "$HOME/.cargo/env"
