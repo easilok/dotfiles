953d14ac227abe7af1e3d5188eabf1a2a61c5f2e
Luis Pereira <luispereira.tkd@gmail.com>
2025-06-23 12:53:20 +0100

    awesome: force non maximized windows by default

--- home/.config/awesome/rules.lua
+++ home/.config/awesome/rules.lua
@@ -8,56 +8,62 @@ local variables = require("variables")
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
     -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
+    {
+        rule = {},
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = awful.client.focus.filter,
+            raise = true,
+            keys = clientkeys,
+            buttons = clientbuttons,
+            screen = awful.screen.preferred,
+            placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+            maximized = false
+        }
     },
 
     -- Floating clients.
-    { rule_any = {
-        instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-          "pinentry",
-        },
-        class = {
-          "Arandr",
-          "Blueman-manager",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-          "Wpa_gui",
-          "veromix",
-          "telegram-desktop",
-          "TelegramDesktop",
-          "xtightvncviewer",
-          "skype",
-          "Skype",
-          "Xfce-appfinder",
-          "mpv",
-          "vlc",
-      },
+    {
+        rule_any = {
+            instance = {
+                "DTA",   -- Firefox addon DownThemAll.
+                "copyq", -- Includes session name in class.
+                "pinentry",
+            },
+            class = {
+                "Arandr",
+                "Blueman-manager",
+                "Gpick",
+                "Kruler",
+                "MessageWin",  -- kalarm.
+                "Sxiv",
+                "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
+                "Wpa_gui",
+                "veromix",
+                "telegram-desktop",
+                "TelegramDesktop",
+                "xtightvncviewer",
+                "skype",
+                "Skype",
+                "Xfce-appfinder",
+                "mpv",
+                "vlc",
+            },
 
-        -- Note that the name property shown in xprop might be set slightly after creation of the client
-        -- and the name shown there might not match defined rules here.
-        name = {
-          "Event Tester",  -- xev.
+            -- Note that the name property shown in xprop might be set slightly after creation of the client
+            -- and the name shown there might not match defined rules here.
+            name = {
+                "Event Tester", -- xev.
+            },
+            role = {
+                "AlarmWindow",   -- Thunderbird's calendar.
+                "ConfigManager", -- Thunderbird's about:config.
+                "pop-up",        -- e.g. Google Chrome's (detached) Developer Tools.
+            }
         },
-        role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "ConfigManager",  -- Thunderbird's about:config.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      }, properties = { floating = true }},
+        properties = { floating = true }
+    },
 
     -- Add titlebars to normal clients and dialogs
     -- { rule_any = {type = { "normal", "dialog" }
@@ -65,37 +71,37 @@ awful.rules.rules = {
     -- },
     -- Keep dialogs on top
     {
-	  rule_any = {
-        class = {"file_progress", },
-        type = { "dialog" },
-      },
-      properties = { ontop = true, },
+        rule_any = {
+            class = { "file_progress", },
+            type = { "dialog" },
+        },
+        properties = { ontop = true, floating = true },
     },
 
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
     {
-        rule_any = { 
-            class = {"Firefox", "firefox", "qutebrowser", "Qutebrowser", "brave-browser", "Brave-browser", "Librewolf" },
+        rule_any = {
+            class = { "Firefox", "firefox", "qutebrowser", "Qutebrowser", "brave-browser", "Brave-browser", "Librewolf" },
         },
-        properties = { tag = tagnames[4] } 
+        properties = { tag = tagnames[4] }
     },
     {
-        rule_any = { 
-            class = {"pcmanfm", "Pcmanfm"},
+        rule_any = {
+            class = { "pcmanfm", "Pcmanfm" },
         },
-        properties = { tag = tagnames[6] } 
+        properties = { tag = tagnames[6] }
     },
     {
-        rule_any = { 
-            class = {"remmina", "Remmina"},
+        rule_any = {
+            class = { "remmina", "Remmina" },
         },
-        properties = { screen = 1, tag = tagnames[7] } 
+        properties = { screen = 1, tag = tagnames[7] }
     },
-    { 
-        rule_any = { 
-            class = {"Microsoft Teams - Preview", "teams-for-linux"},
+    {
+        rule_any = {
+            class = { "Microsoft Teams - Preview", "teams-for-linux" },
         },
-        properties = { screen = 1, tag = tagnames[5] } 
+        properties = { screen = 1, tag = tagnames[5] }
     },
 }
