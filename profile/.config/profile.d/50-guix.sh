560b4f048de74cda914f9547e4145650eab369c3
Luis Pereira <luispereira.tkd@gmail.com>
2025-07-13 23:14:34 +0100

    profile: guix variables only when installed

--- profile/.config/profile.d/50-guix.sh
+++ profile/.config/profile.d/50-guix.sh
@@ -1,35 +1,39 @@
 #!/usr/bin/env bash
 	
-## Extra Guix profiles section
- unset GUIX_PROFILE
-GUIX_EXTRA_PROFILES="$HOME/.guix-extra-profiles"
-if [ -d "$GUIX_EXTRA_PROFILES" ]; then
-    for i in $GUIX_EXTRA_PROFILES/*; do
-	profile=$i/$(basename "$i")
-	if [ -f "$profile"/etc/profile ]; then
-	    GUIX_PROFILE="$profile"
-	    . "$GUIX_PROFILE"/etc/profile
-	fi
-	if [ -d "$profile"/share/man ]; then
-        export MANPATH=$GUIX_PROFILE/share/man${MANPATH:+:}$MANPATH
+if [ -f "$(which guix 2> /dev/null)" ]; then
+
+    ## Extra Guix profiles section
+    unset GUIX_PROFILE
+    GUIX_EXTRA_PROFILES="$HOME/.guix-extra-profiles"
+    if [ -d "$GUIX_EXTRA_PROFILES" ]; then
+        for i in $GUIX_EXTRA_PROFILES/*; do
+        profile=$i/$(basename "$i")
+        if [ -f "$profile"/etc/profile ]; then
+            GUIX_PROFILE="$profile"
+            . "$GUIX_PROFILE"/etc/profile
+        fi
+        if [ -d "$profile"/share/man ]; then
+            export MANPATH=$GUIX_PROFILE/share/man${MANPATH:+:}$MANPATH
+        fi
+        unset profile
+        done
     fi
-	unset profile
-    done
+
+    ## Default Guix
+    GUIX_PROFILE="$HOME/.guix-profile"
+    [ -f "$GUIX_PROFILE/etc/profile" ] && . "$GUIX_PROFILE/etc/profile"
+    GUIX_PROFILE="$HOME/.config/guix/current"
+    [ -f "$GUIX_PROFILE/etc/profile" ] && . "$GUIX_PROFILE/etc/profile"
+
+    export GUIX_PROFILE
+
+    ## SSL CA path set
+    export SSL_CERT_DIR="$GUIX_PROFILE/etc/ssl/certs"
+    # Default to system profile if no ssl certs found
+    [ ! -d "$SSL_CERT_DIR" ] && export SSL_CERT_DIR="$HOME/.guix-profile/etc/ssl/certs"
+
+    export SSL_CERT_FILE="$SSL_CERT_DIR/ca-certificates.crt"
+    export GIT_SSL_CAINFO="$SSL_CERT_FILE"
+    export CURL_CA_BUNDLE="$SSL_CERT_FILE"
+
 fi
-
-## Default Guix
-GUIX_PROFILE="$HOME/.guix-profile"
-[ -f "$GUIX_PROFILE/etc/profile" ] && . "$GUIX_PROFILE/etc/profile"
-GUIX_PROFILE="$HOME/.config/guix/current"
-[ -f "$GUIX_PROFILE/etc/profile" ] && . "$GUIX_PROFILE/etc/profile"
-
-export GUIX_PROFILE
-
-## SSL CA path set
-export SSL_CERT_DIR="$GUIX_PROFILE/etc/ssl/certs"
-# Default to system profile if no ssl certs found
-[ ! -d "$SSL_CERT_DIR" ] && export SSL_CERT_DIR="$HOME/.guix-profile/etc/ssl/certs"
-
-export SSL_CERT_FILE="$SSL_CERT_DIR/ca-certificates.crt"
-export GIT_SSL_CAINFO="$SSL_CERT_FILE"
-export CURL_CA_BUNDLE="$SSL_CERT_FILE"
