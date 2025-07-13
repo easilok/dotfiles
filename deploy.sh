cb8ac93dd220b4a465491c543e8edb652b39e7c8
Luis Pereira <luispereira.tkd@gmail.com>
2025-07-13 23:15:33 +0100

    deploy: more dynamic script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,55 @@
 #!/usr/bin/env bash
 
 [[ -z $(which stow) ]] && echo "stow not found" && exit 1
+[[ -z $HOSTNAME ]] && echo "HOSTNAME environment is required for deploying" && exit 1
 
-stowFolders=(
-    "bookmarks"
-    "i3"
+baseFolders=(
     "nvim"
     "taskwarrior"
-    "wezterm"
+    "starship"
 )
+
+desktopFolders=(
+    "bookmarks"
+    "awesomewm"
+    "picom"
+    "i3"
+    "i3lock"
+    "wezterm"
+    "greenclip"
+)
+
+extraFolders=(
+    "profile"
+)
+
+# stowFolders+=( 
+#     "${desktopFolders[@]}"
+#     "${baseFolders[@]}"
+#     "${extraFolders[@]}"
+# )
+
+case $HOSTNAME in
+  "ackerman")
+      stowFolders=(
+          "awesomewm"
+          "picom"
+          "nvim"
+          "taskwarrior"
+      )
+    ;;
+
+  "cloud-nix")
+      stowFolders=(
+          "nvim"
+          "taskwarrior"
+      )
+    ;;
+  *)
+      echo "Hostname \"$HOSTNAME\" not recognized"
+      exit 1
+    ;;
+esac
 
 
 for folder in ${stowFolders[@]}; do
