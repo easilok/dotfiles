cfcb0d093d61249e1894af0a9fdad6e38b74d521
Luis Pereira <luispereira.tkd@gmail.com>
2025-07-29 16:59:58 +0100

    awesomewm: update temperature plugin

--- awesomewm/.config/awesome/evil/temperature.lua
+++ awesomewm/.config/awesome/evil/temperature.lua
@@ -6,11 +6,11 @@ local awful = require("awful")
 local update_interval = 15
 local temp_script = [[
   sh -c "
-  $HOME/.local/bin/cpu_temperature
+  $HOME/.local/bin/get-cpu-temperature
   "]]
 
 -- Periodically get temperature info
 awful.widget.watch(temp_script, update_interval, function(widget, stdout)
-  local temp = math.floor(stdout / 1000)
+  local temp = math.floor(stdout)
     awesome.emit_signal("evil::temperature", tonumber(temp))
 end)
